f116f39c791d67ef3d99347cac31e2fba7ef660d
komawer02 <komawer02>
2022-06-21 00:37:40 +0900

    markdown

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.set('view engine', 'ejs');
 const MongoClient = require('mongodb').MongoClient;
 
 var db;
@@ -24,6 +24,17 @@ MongoClient.connect('mongodb+srv://root:ASDun0304@cluster0.eatnaco.mongodb.net/?
 app.get('/', (요청, 응답) => {
     응답.sendFile(__dirname + '/index.html')
 })
-app.post('/add', function(요쳥, 응답){
+app.post('/add', function(요청, 응답){
+    응답.send('전송완료');
+    db.collection('post').insertOne({제목 : 요청.body.title, 날짜 : 요청.body.date}, (err, result) =>{
+        console.log('저장완료');
+    })
+})
+app.get('/list', function(요청, 응답){
     
+    db.collection('post').find().toArray(function(err, result){
+        응답.render('list.ejs', {posts : result});
+    });
+    
+
 })
